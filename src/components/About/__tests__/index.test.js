1d1f24739c25f8a59a0276e8bfbb0353077ebc12
Alejandro Robles <arobles2828@gmail.com>
2021-06-26 15:33:35 -0700

    added new code

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,12 +1,7 @@
 import React from 'react';
-
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
-
-const{ asFragment } = render(<About />);
-
-expect(asFragment()).toMatchSnapshot();
 
 afterEach(cleanup);
 describe('About component', () => {
@@ -17,6 +12,9 @@ describe('About component', () => {
 
     // Second Test
     it('matches snapshot DOM node structure', () => {
+      const{ asFragment } = render(<About />);
+
+      expect(asFragment()).toMatchSnapshot();
       // render About
-    })
+    });
 })
